4834b07325683c04202877bddce533b6ae7a6bfd
Durig, Burak <burak.durig@nissan-usa.com>
2024-12-27 17:24:49 -0600

    Initial Commit

---
+++ app/src/main/java/com/mastercoding/contactmanagerapp/Contacts.java
@@ -0,0 +1,52 @@
+package com.mastercoding.contactmanagerapp;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+// if no variable is assigned to @Entity, then it will automatically be assigned to class name
+@Entity(tableName = "contacts")
+public class Contacts {
+    // the below @ColumnInfo will automatically assign table name without using the variable name
+    @ColumnInfo(name = "contact_id")
+    @PrimaryKey(autoGenerate = true)
+    //The above is for SQLight
+    private int id;
+    @ColumnInfo(name = "contact_name")
+    private String name;
+    @ColumnInfo(name = "contact_email")
+    private String email;
+
+    public Contacts(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Contacts() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
